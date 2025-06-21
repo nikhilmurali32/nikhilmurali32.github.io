51844a80928efe54bed74962f7f1bed7e35243bc
nikhilmurali32 <mnikhilbharath05@gmail.com>
2025-06-21 11:42:24 -0400

    Update resume and info

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -1,6 +1,6 @@
 export const aboutData = {
     title: "Who I am",
-    description1: "My name's Nikhil Murali. I'm currently pursuing my masters in computer science at the University at Buffalo. I'm also interning at Empire State Realty Trust as a AI Engineer trying to solve their business problem using AI. I'm also a full stack developer and a machine learning engineer. I'm a quick learner and I'm always looking for new challenges.",
+    description1: "My name's Nikhil Murali. I'm currently pursuing my Masters in Computer Science at the University at Buffalo. I'm also interning at Empire State Realty Trust as a AI Engineer trying to solve their business problem using AI. I'm also a full stack developer and a machine learning engineer. I'm a quick learner and I'm always looking for new challenges.",
     description2: "I'm a quick learner and I'm always looking for new challenges.",
     image: 2
 }
--- src/data/experienceData.js
+++ src/data/experienceData.js
@@ -8,16 +8,23 @@ export const experienceData = [
     },
     {
         id: 2,
+        company: 'SUNY Research Foundation',
+        jobtitle: 'Graduate Research Assistant',
+        startYear: '2024',
+        endYear: 'Present'
+    },
+    {
+        id: 3,
         company: 'Standard Chartered Bank',
         jobtitle: 'Software Engineer',
         startYear: '2021',
         endYear: '2024'
     },
-    {
-        id: 3,
-        company: 'Keolis Mass Rapid Transit',
-        jobtitle: 'Operations Intern',
-        startYear: '2020',
-        endYear: '2020'
-    },
+    // {
+    //     id: 3,
+    //     company: 'Keolis Mass Rapid Transit',
+    //     jobtitle: 'Operations Intern',
+    //     startYear: '2020',
+    //     endYear: '2020'
+    // },
 ]
