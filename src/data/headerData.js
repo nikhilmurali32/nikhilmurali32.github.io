4e47351049061500e0eb9b669664f936deaa2b40
nikhilmurali32 <mnikhilbharath05@gmail.com>
2025-06-21 15:46:07 -0400

    Change name font

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -1,7 +1,7 @@
 import resume from '../assets/pdf/Nikhil_Murali_Resume.pdf'
 
 export const headerData = {
-    name: 'NIKHIL MURALI',
+    name: 'Nikhil Murali',
     title: "AI Engineer & Software Developer",
     desciption:"I'm a quick learner and I'm always looking for new challenges.",
     image: 'https://raw.githubusercontent.com/nikhilmurali32/developer-portfolio/817e6b89fb355f57a4436a320046af8946dd42af/src/assets/png/55f29d04-0117-4fda-9cda-9e0aca17d91b%20Copy-EDIT.jpg',
