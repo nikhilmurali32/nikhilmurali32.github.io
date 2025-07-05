d165df8e1b72004a3c60c4a3a7e1bb23041f7d0a
nikhilmurali32 <mnikhilbharath05@gmail.com>
2025-07-05 10:06:24 -0400

    Update project details

--- src/data/achievementData.js
+++ src/data/achievementData.js
@@ -1,20 +1,20 @@
 export const achievementData = {
-    bio : "Putting quantified achievements on a resume is great, but I just don’t work with hard numbers. I have no data to support the quality of my work. If you have lots of relevant experience, the heading statement of your resume will be a summary.",
+    bio : "Certifications",
     achievements : [
         {
             id : 1,
-            title : 'Google Cloud certification',
-            details : 'Google Cloud certifications validate your expertise and showcase your ability to transform businesses with Google Cloud technology.',
-            date : 'Aug 20, 2019',
-            field : 'Automation',
+            title : 'AWS Certified Solutions Architect - Associate',
+            details : 'AWS certifications validate your expertise and showcase your ability to transform businesses with AWS technology.',
+            // date : 'Aug 20, 2019',
+            field : 'AWS',
             image : 'https://images.unsplash.com/photo-1573141597928-403fcee0e056?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=667&q=80'
         },
         {
             id : 2,
-            title : 'AWS Certification',
-            details : 'Validate technical skills and cloud expertise to grow your career and business.',
-            date : 'Aug 20, 2019',
-            field : 'Automation',
+            title : 'AWS Certified Developer - Associate',
+            details : 'AWS certifications validate your expertise and showcase your ability to transform businesses with AWS technology.',
+            // date : 'Aug 20, 2019',
+            field : 'AWS',
             image : 'https://images.unsplash.com/photo-1523474253046-8cd2748b5fd2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80'
         },
         {
